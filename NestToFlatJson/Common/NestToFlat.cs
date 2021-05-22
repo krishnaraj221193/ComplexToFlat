bea64e27b13378cdd0c4158100a205de1f159b70
krishnaraj221193 <krishnaraj221193@gmail.com>
2021-05-22 11:51:04 +0530

    Add files via upload

---
+++ NestToFlatJson/Common/NestToFlat.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NestToFlatProject
+{
+    public class NestToFlat
+    {
+        public JArray getFlatObject(object requestObj)
+        {
+            JArray jArray = new JArray();
+            try
+            {
+                if (requestObj != null)
+                {
+                    JArray complexObj = JArray.FromObject(requestObj);
+                    complexObj.ToList().ForEach(x =>
+                    {
+                        string routeName = Convert.ToString(x["routename"]);
+                        if(((JObject)x).ContainsKey("stops"))
+                        x["stops"].ToList().ForEach(y =>
+                        {
+                            string stopName = Convert.ToString(y["stopname"]);
+                            if (((JObject)y).ContainsKey("objects"))
+                                y["objects"].ToList().ForEach(z =>
+                                {
+                                    JObject jObject = new JObject();
+                                    JObject.Parse(z.ToString()).Properties().ToList().ForEach(a =>
+                                    {
+                                        jObject.Add(a.Name, a.Value);
+                                    });
+                                    jObject.Add("stopname", stopName);
+                                    jObject.Add("routename", routeName);
+                                    jArray.Add(jObject);
+                                });
+                            else
+                            {
+                                JObject jObject = new JObject();
+                                jObject.Add("stopname", stopName);
+                                jObject.Add("routename", routeName);
+                                jArray.Add(jObject);
+                            }
+                        });
+                        else
+                        {
+                            JObject jObject = new JObject();                                
+                            jObject.Add("routename", routeName);
+                            jArray.Add(jObject);                            
+                        }
+                    });
+                }
+            }catch(Exception ex)
+            {
+
+            }
+            return jArray;
+        }
+    }
+}
---
+++ NestToFlatJson/Controllers/RouteController.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace NestToFlatProject.Controllers
+{
+    public class RouteController : ApiController
+    {
+        NestToFlat _nestToFlat;
+        public RouteController()
+        {
+            _nestToFlat = new NestToFlat();
+        }
+        public IHttpActionResult Post([FromBody]object complexObj)
+        {
+            JArray jArray = _nestToFlat.getFlatObject(complexObj);
+            return Ok(jArray);
+        }
+    }
+}
